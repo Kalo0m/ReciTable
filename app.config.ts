9848363318fdbfab4036ac18938f58e54ca4edcb
Théo Letouzé <theo.letouze@abtasty.com>
2025-04-22 00:31:50 +0200

    chore(deployment): use netlify as deployment preset

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from '@tanstack/react-start/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   tsr: {
     appDirectory: 'src',
+  },
+  server: {
+    preset: 'netlify',
   },
   vite: {
     plugins: [
@@ -12,4 +15,4 @@ export default defineConfig({
       }),
     ],
   },
-})
+});
