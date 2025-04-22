1a790ad78d72ff91ef0b9077722ea0f3ae6460c4
Théo Letouzé <theo.letouze@abtasty.com>
2025-04-22 17:46:32 +0200

    chore: add debug true

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -79,7 +79,7 @@ function RootComponent() {
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
+    <ClerkProvider routerDebug appearance={{ baseTheme: dark }}>
       <html lang="en">
         <head>
           <HeadContent />
